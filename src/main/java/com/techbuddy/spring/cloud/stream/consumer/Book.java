007dde36b7b9150ba98d317d83fa0f2baa0938b6
Ashutosh Kumar <ashutosh.kumar3@conduent.com>
2021-10-18 23:37:56 +0530

    initial commit kafka consumer using spring cloud stream

---
+++ src/main/java/com/techbuddy/spring/cloud/stream/consumer/Book.java
@@ -0,0 +1,15 @@
+package com.techbuddy.spring.cloud.stream.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Book {
+    private String bookName;
+    private String isbn;
+}
